749318d2bb5e7f859ab1421bdce46255ab3e3744
Adeyemo <adeyemodanointed@yahoo.com>
2022-06-01 06:31:00 -0700

    To upper case

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
-def uppercase(c):
-    if(ord(c) < 65 or ord(c) > 90):
-        return False
-    else:
-        return True
+def uppercase(str):
+    strlen = len(str)
+    for i in range(strlen):
+        if(ord(str[i]) > 96 and ord(str[i]) < 123):
+            c = chr(ord(str[i])-32)
+        else:
+            c = str[i]
+        if(i == strlen-1):
+            print("{}".format(c))
+        else:
+            print("{}".format(c), end='')
