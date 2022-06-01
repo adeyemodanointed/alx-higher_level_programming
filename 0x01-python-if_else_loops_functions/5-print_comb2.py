7c1436d080e616fa128710b9ecc24933abc9f5dd
Adeyemo <adeyemodanointed@yahoo.com>
2022-06-01 02:16:19 -0700

    change

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
     if(i < 99):
-        if(i < 10):
-            print("0{:d}".format(i), end=", ")
-        else:
-            print("{:d}".format(i), end=", ")
+            print("{0:0=2d}".format(i), end=", ")
     else:
         print("{:d}".format(i))
