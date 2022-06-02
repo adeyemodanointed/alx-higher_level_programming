91dc4ff92c014b46a06a30d2cffe1b5d421be42e
Adeyemo <adeyemodanointed@yahoo.com>
2022-06-02 09:56:55 -0700

    correction

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,5 +2,5 @@
 if __name__ == "__main__":
     import hidden_4
     for value in dir(hidden_4):
-        if(value.startswith("__")):
+        if(not value.startswith("__")):
             print(value)
