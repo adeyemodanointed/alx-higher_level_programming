feb4b97adb5c7ebf52f71bfadd43c1f7844dd645
Adeyemo <adeyemodanointed@yahoo.com>
2022-06-02 09:37:39 -0700

    argv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-from sys import argv
-argv_l = len(argv)
-if (argv_l == 1):
-    print("{:d} arguments.".format(argv_l-1))
-elif(argv_l == 2):
-    print("{:d} argument:\n{:d}: {}".format(argv_l-1, argv_l-1, argv[1]))
-else:
-    print("{:d} arguments:".format(argv_l-1))
-    for i in range(1, argv_l):
-        print("{:d}: {}".format(i, argv[i]))
+if __name__ == "__main__":
+    from sys import argv
+    argv_l = len(argv)
+    if (argv_l == 1):
+        print("{:d} arguments.".format(argv_l-1))
+    elif(argv_l == 2):
+        print("{:d} argument:\n{:d}: {}".format(argv_l-1, argv_l-1, argv[1]))
+    else:
+        print("{:d} arguments:".format(argv_l-1))
+        for i in range(1, argv_l):
+            print("{:d}: {}".format(i, argv[i]))
