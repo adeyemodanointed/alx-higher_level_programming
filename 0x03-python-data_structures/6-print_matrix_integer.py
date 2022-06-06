3d2394062c4e5e178a3e4b78f384aa6114b79451
Adeyemo <adeyemodanointed@yahoo.com>
2022-06-06 16:33:32 -0700

    advanced

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,4 @@
 def print_matrix_integer(matrix=[[]]):
     for list_t in matrix:
         for idx in range(len(list_t)):
-            if(idx < len(list_t)-1):
-                print("{:d} ".format(list_t[idx]), end=" ")
-            else:
-                print("{:d}".format(list_t[idx]))
+            print(" ".join("{:d}").format(list_t[idx])))
