47a813845a82677cf8f6687e15605ff4be67f614
Adeyemo <adeyemodanointed@yahoo.com>
2022-07-07 09:04:16 -0700

    fix 1

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""A module that contains a LockedClass Class"""
+
+
 class LockedClass:
     """A locked class that only lets the user dynamically create the instance
     attribute 'first_name'"""
