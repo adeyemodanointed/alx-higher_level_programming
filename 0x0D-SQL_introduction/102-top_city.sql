cdbdd3dbc8759fb7cb0f241c89c91816e79b51f0
Adeyemo <adeyemodanointed@yahoo.com>
2022-08-11 13:01:15 -0700

    advanced

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,3 @@
+-- displays the first three average temperature by city ordered by temperature (descending)
+
+SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month >= 7 AND month <= 8 GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
