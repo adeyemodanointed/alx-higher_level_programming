c3939d2f0b0a7b4b7e2fdfdcb8b3984bf540671c
Adeyemo Peter <adeyemo@tm30.net>
2022-09-08 00:42:24 +0100

    semistandard

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -4,9 +4,9 @@ const Square1 = require('./5-square');
 module.exports = class Square extends Square1 {
   charPrint (c) {
     if (c === undefined) {
-      this.print()
+      this.print();
     } else {
-      for(let i = 0; i < this.width; i++) {
+      for (let i = 0; i < this.width; i++) {
         console.log(c.repeat(this.height));
       }
     }
