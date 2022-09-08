d240de04d55e37dc0d7517421357aee75ff5485f
Adeyemo Peter <adeyemo@tm30.net>
2022-09-08 01:13:48 +0100

    correction

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,7 +3,7 @@
 let nextMax = 0;
 const args = process.argv.slice(2);
 if (args.length > 1) {
-  args = args.map(el => parseInt(el));
+  args.map(el => parseInt(el));
   args.sort();
   nextMax = args[args.length - 2];
 }
