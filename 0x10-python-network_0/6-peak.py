460ab59450ff0b35229b1bcfa6af9fccb0b49d83
Adeyemo Peter <adeyemo@tm30.net>
2022-09-23 13:36:09 +0100

    revert

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -8,4 +8,9 @@ def find_peak(list_of_integers):
     lo = len(li)
     if lo == 0:
         return
-    return li[lo-1]
+    m = lo // 2
+    if (m == lo - 1 or li[m] >= li[m + 1]) and (m == 0 or li[m] >= li[m - 1]):
+        return li[m]
+    if m != lo - 1 and li[m + 1] > li[m]:
+        return find_peak(li[m + 1:])
+    return find_peak(li[:m])
