6a1386d04d0ba6d521c8c2eaa55e42c7dcd37fa1
adeyemodanointed <adeyemodanointed@yahoo.com>
2022-12-06 21:08:51 +0000

    task 4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
 """
-Write a script that lists all states with a name
-starting with N (upper N) from the database hbtn_0e_0_usa:
-Your script should take 3 arguments: mysql username,
-mysql password and database name (no argument validation needed)
+Write a script that lists all cities from the database hbtn_0e_4_usa
+
+Your script should take 3 arguments:
+mysql username, mysql password and database name
 You must use the module MySQLdb (import MySQLdb)
-Your script should connect to a MySQL server running on localhost at port 3306
-Results must be sorted in ascending order by states.id
+Your script should connect to a
+MySQL server running on localhost at port 3306
+Results must be sorted in ascending order by cities.id
+You can use only execute() once
 Results must be displayed as they are in the example below
 Your code should not be executed when imported
 """
+
 if __name__ == "__main__":
     import MySQLdb
     from sys import argv
@@ -17,14 +20,14 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=argv[1],
                          passwd=argv[2], db=argv[3])
     cur = db.cursor()
+
     try:
-        cur.execute("SELECT * FROM states WHERE name REGEXP '^[N]'")
+        cur.execute("SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states WHERE state_id = states.id")
         rows = cur.fetchall()
+        for row in rows:
+            print(row)
     except MySQLdb.Error as e:
         print("MySQL Error: %s" % str(e))
-
-    for row in rows:
-        print(row)
 
     cur.close()
     db.close()
