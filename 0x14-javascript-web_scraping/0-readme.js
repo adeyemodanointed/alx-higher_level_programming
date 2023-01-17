65b300dbbcfb4e474b780cee409219117682fc1b
adeyemodanointed <adeyemodanointed@yahoo.com>
2023-01-17 22:42:43 +0000

    task 0 correction

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,6 @@
 #!/usr/bin/node
-
-const request = require('request');
 const fs = require('fs');
 
 fs.readFile(process.argv[2], 'utf-8', (error, data) => {
-	if (error)
-		console.log(error);
-	if (data)
-		console.log(data)
-})
+  if (error) { console.log(error); } else { console.log(data); }
+});
